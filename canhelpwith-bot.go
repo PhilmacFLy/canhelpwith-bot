132417a75b5b843444e22d968bf9be87aad053dd
Philipp Neumann <philipp.neumann@autodesk.com>
2018-07-17 16:50:24 +0200

    First working version with horrible design

---
+++ canhelpwith-bot.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/McKael/madon"
+	"github.com/blevesearch/bleve"
+	"github.com/gorilla/mux"
+)
+
+var index bleve.Index
+var client *madon.Client
+
+type config struct {
+	AppName        string
+	Webpage        string
+	Permissions    []string
+	Instance       string
+	AppID          string
+	AppSecret      string
+	Username       string
+	Password       string
+	Hashtags       []string
+	HashtagScanned map[string]int64
+	Adress         string
+}
+
+type status struct {
+	Name    string
+	Message template.HTML
+	Score   float64
+	URL     string
+}
+
+func loadConfig(path string) (config, error) {
+	var c config
+	var filename string
+	if path != "" {
+		filename = path
+	} else {
+		filename = "config.json"
+	}
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return c, err
+	}
+	err = json.Unmarshal(body, &c)
+
+	return c, err
+
+}
+
+func saveConfig(c config, path string) error {
+	var filename string
+	if path != "" {
+		filename = path
+	} else {
+		filename = "config.json"
+	}
+	j, err := json.Marshal(&c)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filename, j, 755)
+	return err
+}
+
+func addStatitoIndex(c config, client *madon.Client) {
+	var stati []madon.Status
+	for _, h := range c.Hashtags {
+		var lp madon.LimitParams
+		last := c.HashtagScanned[h]
+		if last != 0 {
+			lp.All = false
+			lp.SinceID = last
+		} else {
+			lp.All = true
+		}
+		st, err := client.GetTimelines(h, false, false, &lp)
+		if err != nil {
+			log.Println("Couldnt load Stati:", err)
+		} else {
+			stati = append(stati, st...)
+		}
+		if len(st) > 0 {
+			c.HashtagScanned[h] = st[0].ID
+		}
+		err = saveConfig(c, "")
+		if err != nil {
+			fmt.Println("Error saving config:", err)
+		}
+	}
+	for _, s := range stati {
+		index.Index(strconv.FormatInt(s.ID, 10), s)
+	}
+
+}
+
+func showtemplate(w http.ResponseWriter, path string, data interface{}) {
+	t, err := template.ParseFiles(path)
+	if err != nil {
+		fmt.Fprintln(w, "Error parsing template:", err)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		fmt.Fprintln(w, "Error executing template:", err)
+		return
+	}
+}
+
+func resultHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.FormValue("query")
+	query := bleve.NewQueryStringQuery(q)
+	searchRequest := bleve.NewSearchRequest(query)
+	searchRequest.Highlight = bleve.NewHighlight()
+	searchResult, err := index.Search(searchRequest)
+	if err != nil {
+		fmt.Fprintln(w, "Error executing search:", err)
+	}
+	searchRequest.SortBy([]string{"-_score"})
+	var stati []status
+	for _, res := range searchResult.Hits {
+		var st status
+		id, err := strconv.ParseInt(res.ID, 10, 64)
+		if err != nil {
+			fmt.Println(w, "Error parsing Integer:", err)
+			return
+		}
+		s, err := client.GetStatus(id)
+		if err != nil {
+			fmt.Println(w, "Error getting Status:", err)
+			return
+		}
+		st.Name = s.Account.DisplayName + " / " + s.Account.Username
+		st.URL = s.URL
+		st.Message = template.HTML(s.Content)
+		st.Score = res.Score
+		stati = append(stati, st)
+	}
+	showtemplate(w, "templates/result.html", stati)
+}
+
+func frontendHandler(w http.ResponseWriter, r *http.Request) {
+	a := r.FormValue("action")
+	switch a {
+	case "search":
+		resultHandler(w, r)
+	default:
+		showtemplate(w, "templates/search.html", nil)
+	}
+}
+
+func main() {
+	var err error
+
+	_, err = os.Stat("toot.bleve")
+	if os.IsNotExist(err) {
+		log.Println("Creating Search Index")
+		mapping := bleve.NewIndexMapping()
+		index, err = bleve.New("toot.bleve", mapping)
+		if err != nil {
+			log.Fatal("Error creating Index:", err)
+		}
+	} else {
+		if err == nil {
+			index, err = bleve.Open("toot.bleve")
+			if err != nil {
+				log.Fatal("Error loading Index:", err)
+			}
+		} else {
+			log.Fatal("Error checking for Index:", err)
+		}
+	}
+
+	log.Println("Loading Config")
+	c, err := loadConfig("")
+	if err != nil {
+		log.Fatal("Error loading Config:", err)
+	}
+	if c.HashtagScanned == nil {
+		c.HashtagScanned = make(map[string]int64)
+	}
+	if (c.AppID != "") && (c.AppSecret != "") {
+		client, err = madon.NewApp(c.AppName, c.Webpage, c.Permissions, madon.NoRedirect, c.Instance)
+		if err != nil {
+			log.Fatal("Error creating App on Instance:", err)
+		}
+		c.AppID = client.ID
+		c.AppSecret = client.Secret
+	} else {
+
+		client, err = madon.RestoreApp(c.AppName, c.Instance, c.AppID, c.AppSecret, nil)
+		if err != nil {
+			log.Fatal("Error creating App on Instance:", err)
+		}
+	}
+	log.Println("Created App. Logging in now.")
+	err = client.LoginBasic(c.Username, c.Password, c.Permissions)
+	if err != nil {
+		log.Fatal("Failed to log into accout:", err)
+	}
+
+	log.Println("Performing initial Scan")
+	addStatitoIndex(c, client)
+
+	log.Println("Starting Scan Timer")
+	ticker := time.NewTicker(1 * time.Minute)
+	go func(c config, client *madon.Client) {
+		for range ticker.C {
+			addStatitoIndex(c, client)
+		}
+	}(c, client)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", frontendHandler)
+	log.Fatal(http.ListenAndServe(c.Adress, r))
+}
